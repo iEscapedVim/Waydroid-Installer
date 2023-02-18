0a07c27d41026bed3acbc0e7bfd4edf1c8221816
ZERO <Yasir.RWebDesigner@gmail.com>
2023-02-19 01:51:47 +0500

    Update install.sh
    Updated the whole tested it a few times works perfect.

--- install.sh
+++ install.sh
@@ -1,75 +1,97 @@
 #!/bin/bash
-
-# Check if the script is run as root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
+# Check if yay is installed
+if ! command -v yay &> /dev/null; then
+    read -p "yay is not installed. Do you want to install it? (y/n) " choice
+    case $choice in
+        [Yy]* )
+ 	   # Install yay
+ 	   sudo pacman -S --needed --noconfirm git base-devel xdg-utils mailcap
+ 	   git clone https://aur.archlinux.org/yay-bin.git
+ 	   cd yay-bin
+	   makepkg -si --noconfirm
+  	   cd ..
+   	   rm -rf yay-bin
+            echo "yay has been installed."
+            ;;
+        * )
+            echo "yay is required for this script to run. Exiting."
+            exit 1
+            ;;
+    esac
 fi
+
+yay -S --noconfirm python-pyclip dkms android-tools
 
 # Determine the current kernel
 current_kernel=$(uname -r)
 echo "Current kernel version is $current_kernel"
 
-# Check if the current kernel is supported
-if ! pacman -Qs "linux" > /dev/null && ! pacman -Qs "linux-lts" > /dev/null && ! pacman -Qs "linux-zen" > /dev/null && ! pacman -Qs "linux-xanmod-anbox" > /dev/null ; then
-    echo "This script only supports linux linux-lts, linux-zen, and linux-xanmod-anbox kernels"
-    exit 1
-fi
+# Prompt the user to choose which kernel headers to install
+echo "Which kernel headers do you want to install?"
+echo "1. linux-headers"
+echo "2. linux-lts-headers"
+echo "3. linux-zen-headers"
+echo "4. linux-xanmod-headers"
+read -p "Enter your choice (1-4): " choice
 
-# Install required dependencies
-pacman -Syyu --noconfirm yay
-yay -S --noconfirm dkms android-tools
-
-# Install kernel headers if needed
-if pacman -Qs "linux" > /dev/null ; then
-    if pacman -Qs "linux-headers" > /dev/null ; then
-        echo "linux-headers is already installed"
-    else
-        pacman -S --noconfirm linux-headers
-    fi
-    dkms install binder_linux/1.3.1 -k $current_kernel
-    dkms install ashmem_linux/1.3.1 -k $current_kernel
-elif pacman -Qs "linux-lts" > /dev/null ; then
-    if pacman -Qs "linux-lts-headers" > /dev/null ; then
-        echo "linux-lts-headers is already installed"
-    else
-        pacman -S --noconfirm linux-lts-headers
-    fi
-    dkms install binder_linux/1.3.1 -k $current_kernel
-    dkms install ashmem_linux/1.3.1 -k $current_kernel
-elif pacman -Qs "linux-zen" > /dev/null ; then
-    if pacman -Qs "linux-zen-headers" > /dev/null ; then
-        echo "linux-zen-headers is already installed"
-    else
-        pacman -S --noconfirm linux-zen-headers
-elif pacman -Qs "linux-xanmod-anbox" > /dev/null ; then
-    if pacman -Qs "linux-xanmod-headers" > /dev/null ; then
-        echo "linux-xanmod-headers is already installed"
-    else
-        yay -S --noconfirm linux-xanmod-anbox linux-xanmod-headers
-    fi
-    dkms install binder_linux/1.3.1 -k $current_kernel-xanmodanbox
-    dkms install ashmem_linux/1.3.1 -k $current_kernel-xanmodanbox
-else
-    echo "Unsupported kernel"
-    exit 1
-fi
+case $choice in
+    1)
+        if pacman -Qs "linux-headers" > /dev/null ; then
+            echo "linux-headers is already installed"
+        else
+          sudo pacman -S --noconfirm linux-headers
+        fi
+         sudo dkms install binder_linux/1.3.1 -k $current_kernel
+         sudo dkms install ashmem_linux/1.3.1 -k $current_kernel
+        ;;
+    2)
+        if pacman -Qs "linux-lts-headers" > /dev/null ; then
+            echo "linux-lts-headers is already installed"
+        else
+           sudo pacman -S --noconfirm linux-lts-headers
+        fi
+       sudo dkms install binder_linux/1.3.1 -k $current_kernel
+       sudo dkms install ashmem_linux/1.3.1 -k $current_kernel
+        ;;
+    3)
+        if pacman -Qs "linux-zen-headers" > /dev/null ; then
+            echo "linux-zen-headers is already installed"
+        else
+           sudo pacman -S --noconfirm linux-zen-headers
+        fi
+        ;;
+    4)
+        if pacman -Qs "linux-xanmod-headers" > /dev/null ; then
+            echo "linux-xanmod-headers is already installed"
+        else
+            yay -S --noconfirm linux-xanmod-anbox linux-xanmod-anbox-headers
+        fi
+       sudo dkms install binder_linux/1.3.1 -k $current_kernel
+       sudo dkms install ashmem_linux/1.3.1 -k $current_kernel
+        ;;
+    *)
+        echo "Invalid choice"
+        exit 1
+        ;;
+esac
 
 # Install Waydroid
-yay -S --noconfirm waydroid
+echo "Installing Waydroid Waydroid Extra/Script and Waydroid Settings"
+yay -S --noconfirm waydroid-git waydroid-script-git waydroid-settings-git
 
 # Run Waydroid init
 echo "Do you want to run Waydroid init? (y/n)"
 read run_init
 
 if [[ $run_init =~ ^[Yy]$ ]]; then
-    echo "Which version of Waydroid do you want to install? (Vanilla or with GAPPS)"
-    echo "Vanilla version doesn't include Google Apps, while the version with GAPPS does"
+    echo "Which version of Waydroid do you want to install? Vanilla(v) or GAPPS(g)"
     read waydroid_version
     if [[ $waydroid_version =~ ^[Gg]$ ]]; then
-        waydroid init -s GAPPS -f
+       sudo waydroid init -s GAPPS -f
+    elif [[ $waydroid_version =~ ^[Vv]$ ]]; then
+       sudo waydroid init -f
     else
-        waydroid init -f
+       echo "Invalid option entered. Please enter 'v' for Vanilla or 'g' for GAPPS."
     fi
 fi
 
